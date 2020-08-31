a493ce4132714ce27c12c4857f831899c086e982
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-08-31 19:58:56 -0300

    Aula03: O comando if

---
+++ aula3/if.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 10;
+$b = 1;
+
+if ($a > $b)
+  echo "a maior que b";
+
+echo '<br>';
+if ($a > $b) {
+    echo "a maior que b";
+    echo '<br>';
+}
+  
+
+?>
---
+++ aula3/if_else.php
@@ -0,0 +1,9 @@
+<?php
+$a = 10;
+$b = 1;
+if ($a > $b) {
+  echo "a maior que  b";
+} else {
+  echo "a não é maior que b";
+}
+?>
