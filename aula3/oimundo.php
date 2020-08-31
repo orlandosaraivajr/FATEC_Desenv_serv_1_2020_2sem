63ce8a08c154471139c16231ad3081777797bdf6
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-08-31 19:42:06 -0300

    Aula03: Oi mundo

---
+++ aula3/oimundo.php
@@ -0,0 +1,12 @@
+<?php
+echo "Hello world<br>";
+/*
+Salvar o oimundo.php
+Na pasta c:\xampp\htdocs\oimundo.php
+Acessar o navegador o endereço
+http://127.0.0.1/oimundo.php
+*/
+echo "última instrução";
+
+// o script termina aqui, sem tag de fechamento PHP
+?>
